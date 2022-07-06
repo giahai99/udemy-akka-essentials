20977c92b6ed50f0e2cd62c6caf2ec381b21d76f
giahai1999 <hai.truonggia@emea.nttdata.com>
2022-07-06 18:10:15 +0700

    second commit

---
+++ src/main/scala/part1recap/AdvancedRecap.scala
@@ -0,0 +1,12 @@
+package part1recap
+
+object AdvancedRecap extends App {
+
+  // partial functions
+  val partialFunction: PartialFunction[Int, Int] = {
+    case 1 => 42
+    case 2 => 65
+    case 5 => 999
+  }
+
+}
