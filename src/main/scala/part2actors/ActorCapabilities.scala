772aaea65fc15dae3b6ffd83e19813da50282f19
giahai1999 <hai.truonggia@emea.nttdata.com>
2022-07-11 18:23:45 +0700

    test

---
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -0,0 +1,51 @@
+package part2actors
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+object ActorCapabilities extends App {
+
+  class SimpleActor extends Actor {
+    override def receive: Receive = {
+      case "Hi!" => sender() ! "Hello, there" // replying to a message
+      case message: String => println(s"[${self}] I have received: $message")
+      case number: Int => println(s"[Simple actor] I have received a NUMBER: $number")
+      case SpecialMessage(contents) => println(s"[Simple actor] I have received something SPECIAL: $contents")
+      case SendMessageToYourself(content) =>
+        self ! content
+      case SayHiTo(ref) => ref ! "Hi!"
+    }
+  }
+
+  val system = ActorSystem("actorCapabilitiesDemo")
+  val simpleActor = system.actorOf(Props[SimpleActor], "simpleActor")
+
+  simpleActor ! "hello, actor"
+
+  // 1 - messages can be of any type
+  // a) messages must be IMMUTABLE
+  // b) messages must be SERIALIZABLE
+  // in practice use case classes and case objects
+  simpleActor ! 42
+
+  case class SpecialMessage(contents: String)
+  simpleActor ! SpecialMessage("some special content")
+
+  // 2 - actors have information about their context and about themselves
+  // context.self === `this` in OOP
+
+  case class SendMessageToYourself(content: String)
+  simpleActor ! SendMessageToYourself("I am an actor and I am proud of it")
+
+  // 3 -actors can REPLY to messages
+  val alice = system.actorOf(Props[SimpleActor], "alice")
+  val bob = system.actorOf(Props[SimpleActor], "bob")
+
+  case class SayHiTo(ref: ActorRef)
+  alice ! SayHiTo(bob)
+
+  // 4 - dead letters
+  alice ! "Hi!" // reply to "me"
+
+  //
+
+}
